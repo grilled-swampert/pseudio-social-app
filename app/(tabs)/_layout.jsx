b7a4b1740d385a33f7bc26679f9e42482855aa17
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-10-13 11:00:32 +0530

    home page

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,113 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, icon_name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        style={{
+          width: 24,
+          height: 24,
+          tintColor: color,
+          className: "w-6 h-6",
+        }}
+      />
+      <Text
+        style={{
+          fontFamily: focused ? "font-psemibold" : "font-pregular",
+          fontSize: 10,
+          color: color,
+        }}
+      >
+        {icon_name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#4B3F30",
+          tabBarInactiveTintColor: "#A68A64",
+          tabBarStyle: {
+            backgroundColor: "#EDE3D3",
+            borderTopWidth: 0,
+            height: 60,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={icons.home} 
+                color={color}
+                icon_name="Home"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={icons.plus} 
+                color={color}
+                icon_name="Create"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={icons.profile} 
+                color={color}
+                icon_name="Profile"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                focused={focused}
+                icon={icons.bookmark} 
+                color={color}
+                icon_name="Bookmark"
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+import { images } from "../constants";
+
+const Welcome = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-300">
-      <Text className="text-3xl">Working</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" className="text-blue-600">
-        Profile
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Pseudio</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Pseudio
+          </Text>
+
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
-}
+};
+
+export default Welcome;
